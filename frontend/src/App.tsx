c257688aeb0845d06c72d08dcfe48aa81b089d77
Dominika Bomba <d.bomba806@gmail.com>
2026-02-20 19:12:17 +0100

    NavBar: Icons, blocks,
    styles:
    colors palette, <- in _variable.scss, configured in vite.config.ts
    font (from google),
    vite.config.ts CHANGED!!
    
    extra:
    scss <- package.json

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from "./components/Navbar";
+import {Route, Routes} from "react-router";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=bar_chart_4_bars,home_app_logo,man,notifications,settings"/>
+
+            <Navbar />
+            <Routes>
+
+            </Routes>
+
+        </>
+    )
 }
 
 export default App
