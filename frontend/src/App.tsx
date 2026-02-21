815537a42afa62c21c405ce3d97a476858bd60dc
Dominika Bomba <d.bomba806@gmail.com>
2026-02-21 17:17:15 +0100

    Login page added Style updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,22 @@
 import Navbar from "./components/Navbar";
-import {Route, Routes} from "react-router";
+import {useLocation, Route, Routes} from "react-router";
+import Login from "./scenes/Login";
 
 
 function App() {
+
+    const location = useLocation();
+
+    const showNavbar = location.pathname !== "/login";
     return (
         <>
 
             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=bar_chart_4_bars,home_app_logo,man,notifications,settings"/>
 
-            <Navbar />
+            {showNavbar && <Navbar />}
+
             <Routes>
+                <Route path="/login"  element={<Login/>} />
 
             </Routes>
 
