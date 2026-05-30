98337368ce7a71ea8fb4b97fcbe9776131f1987b
Nikodem Jokiel <medokin78@gmail.com>
2026-05-30 20:59:50 +0200

    removed tanstack devtools; added [npm run web] script to start web application

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.scss'
 import App from './App.tsx'
 import {BrowserRouter} from "react-router";
-import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {AuthProvider} from "./AuthContext.tsx";
 
@@ -17,8 +16,8 @@ createRoot(document.getElementById('root')!).render(
                 <App />
             </BrowserRouter>
             </AuthProvider>
-            <ReactQueryDevtools/>
         </QueryClientProvider>
 
     </StrictMode>,
 )
+
